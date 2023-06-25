123ddbd3d8f8d4bdbd65e7dde04f04b0678890f2
1nvaderZim <109631508+1nvaderZim@users.noreply.github.com>
2023-06-25 09:36:08 +0100

    Initialize

---
+++ LCC4_Vehicle_Challenge/Assets/Scripts/IObserver.cs
@@ -0,0 +1,8 @@
+
+
+public interface IObserver
+{
+
+    void OnNotify(string message = "");
+
+}
---
+++ LCC4_Vehicle_Challenge/Assets/Scripts/ISubject.cs
@@ -0,0 +1,10 @@
+
+
+public interface ISubject
+{
+    void AddObserver(IObserver observer);
+
+    void RemoveObserver(IObserver observer);
+
+    void Notify();
+}
